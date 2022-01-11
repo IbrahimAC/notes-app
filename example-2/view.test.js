7435e5d35ac1517daf9dfd36a6ba56a8e3a84092
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2022-01-11 17:08:48 +0000

    updated view.test.js and DOMModelling.md

--- example-2/view.test.js
+++ example-2/view.test.js
@@ -4,13 +4,16 @@
 
 const fs = require('fs');
 const View = require('./view');
-
 describe('Page view', () => {
   it('displays 2 paragraphs', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
-
     const view = new View();
-
     expect(document.querySelectorAll('p').length).toBe(2);
+  });
+  it('adds a paragraph', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+    view.addParagraph();
+    expect(document.querySelectorAll('p').length).toEqual(3);
   });
 });
