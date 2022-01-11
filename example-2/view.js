1c88b5024ddb67e55d5c44cac3d2b55c8028060a
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2022-01-11 18:16:58 +0000

    notesView.test.js and notesView class

--- example-2/view.js
+++ example-2/view.js
@@ -1,9 +1,14 @@
 class View {
   constructor() {
     this.mainContainerEl = document.querySelector('#main-container');
-
     console.log(this.mainContainerEl);
   }
+  addParagraph() {
+    let p = document.createElement("p");
+    p.innerText = 'This paragraph has been dynamically added by JavaScript!';
+    this.mainContainerEl.append(p);
+  }
 }
-
 module.exports = View;
+// const view = new View()
+// console.log(view.addParagraph())
