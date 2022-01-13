c2e8103b754bf943611da07ebe535cc3ce3b9116
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2022-01-13 14:47:31 +0000

    steps 10, 11 completed

--- example-3/messageView.js
+++ example-3/messageView.js
@@ -1,19 +1,33 @@
 class MessageView {
   constructor() {
     this.buttonEl = document.querySelector('#show-message-button');
+    this.hideButtonEl = document.querySelector('#hide-message-button');
     this.mainContainerEl = document.querySelector('#main-container');
 
     this.buttonEl.addEventListener('click', () => {
        this.displayMessage();
     });
+
+    this.hideButtonEl.addEventListener('click', () => {
+      this.hideMessage();
+   });
   }
 
   displayMessage() {
     console.log('Thanks for clicking me!');
     let messageEl = document.createElement('div');
-    messageEl.append(this.value)
+    messageEl.append(this.value = 'This is a message displayed by JavaScript')
     messageEl.setAttribute("id", "message");
     this.mainContainerEl.append(messageEl);
+    
+  }
+
+  hideMessage() {
+    console.log('Message is now hidden!')
+    let message = document.getElementById('message')
+    //message.setAttribute("id", "");
+    message.parentElement.removeChild(message);
+  
   }
 }
 module.exports = MessageView;
--- example-3/messageView.test.js
+++ example-3/messageView.test.js
@@ -13,8 +13,20 @@ describe('MessageView', () => {
     const buttonEl = document.querySelector('#show-message-button');
     buttonEl.click();
     view.displayMessage();
-    
-
     expect(document.querySelector('#message')).not.toBeNull();
+  });
+
+  it('hides the message', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new MessageView();
+    const hideButtonEl = document.querySelector('#hide-message-button');
+    const buttonEl = document.querySelector('#show-message-button');
+
+    buttonEl.click();
+    view.displayMessage();
+
+    hideButtonEl.click();
+    view.hideMessage();
+    expect(document.querySelector('message')).toBeNull();
   });
 });
